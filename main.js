de07685f94241a7758b580eff814418034354293
frederikvera <frederikneuchs96@gmail.com>
2022-10-10 10:56:46 +0200

    Updated charts with new data

--- main.js
+++ main.js
@@ -56,3 +56,84 @@ let chart1 = new Chart(birdStrikeMonth, {
     },
     options: {}
 });
+
+
+
+
+
+
+let birdStrikeCount = document.getElementById('chart2').getContext('2d');
+let chart2 = new Chart(birdStrikeCount, {
+    type: 'pie', //forskellige former af charts
+    data: {
+        labels:['1', '2-10', '11-100', 'More than 100'],
+        datasets:[{
+            label: 'Amount of birds hits per incident',
+            data: [
+                5272,
+                564,
+                27,
+                2,
+
+            ],
+
+            backgroundColor:[
+                'rgba(140, 159, 64, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+
+
+            ],
+            borderColor: [
+                'rgba(140, 159, 64, 0.2)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 206, 86, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {}
+});
+
+
+
+
+
+
+
+
+let birdStrikeSize = document.getElementById('chart3').getContext('2d');
+let chart3 = new Chart(birdStrikeSize, {
+    type: 'bar', //forskellige former af charts
+    data: {
+        labels:['Small', 'Medium', 'Large'],
+        datasets:[{
+            label: 'Size of birds that hit planes',
+            data: [
+                3336,
+                1857,
+                335,
+
+            ],
+
+            backgroundColor:[
+                'rgba(140, 159, 64, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+
+
+            ],
+            borderColor: [
+                'rgba(140, 159, 64, 0.2)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
+
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {}
+});
+
