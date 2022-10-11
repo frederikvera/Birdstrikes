46f744a853b998c41d4d715cbfcdcbcb45c91b67
frizzy1209 <kloecker1997@live.dk>
2022-10-11 10:42:55 +0200

    Updated layout on website

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
+
+// First chart
+// bar chart
+// Birdstrikes per month
+
 let birdStrikeMonth = document.getElementById('chart1').getContext('2d');
 let chart1 = new Chart(birdStrikeMonth, {
-    type: 'bar', //forskellige former af charts
+    type: 'bar',
     data: {
         labels:['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'],
         datasets:[{
-            label: 'Bird Strikes by Month',
+            label: 'Bird Strikes per Month',
             data: [
                 93,
                 128,
@@ -18,7 +23,6 @@ let chart1 = new Chart(birdStrikeMonth, {
                 615,
                 289,
                 147
-
             ],
 
             backgroundColor:[
@@ -34,8 +38,6 @@ let chart1 = new Chart(birdStrikeMonth, {
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(140, 159, 64, 0.2)',
                 'rgba(140, 159, 64, 0.2)',
-
-
             ],
             borderColor: [
                 'rgba(140, 159, 64, 1)',
@@ -54,47 +56,30 @@ let chart1 = new Chart(birdStrikeMonth, {
             borderWidth: 1
         }]
     },
-    options: {}
-});
-
-
-
-
-
-
-let birdStrikeCount = document.getElementById('chart2').getContext('2d');
-let chart2 = new Chart(birdStrikeCount, {
-    type: 'pie', //forskellige former af charts
-    data: {
-        labels:['1', '2-10', '11-100', 'More than 100'],
-        datasets:[{
-            label: 'Amount of birds hits per incident',
-            data: [
-                5272,
-                564,
-                27,
-                2,
-
-            ],
-
-            backgroundColor:[
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(140, 159, 64, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-
-
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(140, 159, 64, 1)',
-                'rgba(255, 206, 86, 1)',
-            ],
-            borderWidth: 1
-        }]
+    options: {
+        scales: {
+            x:{
+                grid:{
+                    display: false
+                }
+            },
+            y:{
+                grid:{
+                    display: false
+                }
+            },
+        },
+        plugins:{
+            legend:{
+                labels:{
+                    font:{
+                        size: 25
+                    },
+                    boxWidth: 0
+                },
+            },
+        },
     },
-    options: {}
 });
 
 
@@ -102,15 +87,18 @@ let chart2 = new Chart(birdStrikeCount, {
 
 
 
+// Second chart
+// Bar chart
+// The amount of birds getting hit per incident
 
 
 let birdStrikeSize = document.getElementById('chart3').getContext('2d');
 let chart3 = new Chart(birdStrikeSize, {
-    type: 'bar', //forskellige former af charts
+    type: 'bar',
     data: {
         labels:['Small', 'Medium', 'Large'],
         datasets:[{
-            label: 'Size of birds that hit planes',
+            label: 'Size of the Birds that Collide with Planes',
             data: [
                 3336,
                 1857,
@@ -134,6 +122,84 @@ let chart3 = new Chart(birdStrikeSize, {
             borderWidth: 1
         }]
     },
-    options: {}
+    options: {
+        scales: {
+            x:{
+                grid:{
+                    display: false
+                }
+            },
+            y:{
+                grid:{
+                    display: false
+                }
+            },
+        },
+        plugins:{
+            legend:{
+                labels:{
+                    font:{
+                        size: 25,
+                    },
+                    boxWidth: 0
+                },
+            },
+        },
+    }
 });
 
+
+
+
+
+
+// Second chart
+// Pie chart
+//
+
+let birdStrikeCount = document.getElementById('chart2').getContext('2d');
+let chart2 = new Chart(birdStrikeCount, {
+    type: 'pie',
+    data: {
+        labels:['1', '2-10', '11-100', 'More than 100'],
+        datasets:[{
+            label: '',
+            data: [
+                5272,
+                564,
+                27,
+                2,
+            ],
+
+            backgroundColor:[
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(140, 159, 64, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+
+
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(140, 159, 64, 1)',
+                'rgba(255, 206, 86, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        plugins:{
+            legend:{
+                position: "right",
+                title: {
+                    display: true,
+                    text: `Birds hit per incident`,
+                    font:{
+                        size: 25
+                    }
+                }
+            }
+        }
+    }
+});
